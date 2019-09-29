c41634ebdc4396a9f8eb7e0c66bbaf4b10a83299
Dylan Strohschein <dylanstrohschein@gmail.com>
2019-09-29 13:44:02 -0400

    Added required comments

--- TextFormat.c
+++ TextFormat.c
@@ -2,35 +2,58 @@
 #include <stdio.h>
 #include <string.h>
 
+//This function will load the next line
+//from stdin with a max of 100 characters
 char* LoadNextLine(char** output){
   return fgets(*output, 101, stdin);
 }
 
+//This function will print
+//the buffer to stdout and format it correctly.
 int PrintBuffer(char* buffer, int justified, int numSpaces, int lineLength){
+  //If it is justified then
+  //we need to format it/
   if(justified == 1){
 
+    //Check how many spaces we have in the line
     int remainingSpace = lineLength - strlen(buffer) + numSpaces;
     int sizeOfSpace = remainingSpace / numSpaces;
     int charactersUsed = 0;
 
+    //Loop through each character in the buffer
     for(int i = 0; i < strlen(buffer); i++){
+
+      //if we have spaces left to delegate then
+      //calculate what the size of a space should be
       if(numSpaces != 0){
         sizeOfSpace = (remainingSpace - charactersUsed) / numSpaces;
       }
+
+      //If the current character is a space
       if(buffer[i] == ' '){
         float x;
         numSpaces--;
+        //print to the screen the number of spaces
+        //that we need to fill the line
         for(x = 0; x < sizeOfSpace; x++){
           printf(" ");
           charactersUsed++;
         }
       }else{
+        //If the currect character is
+        //not a space then just print it
+        //to the screen.
         printf("%c", buffer[i]);
       }
     }
-
+    //After we have looped through
+    //all the characters print a new
+    //line character.
     printf("\n");
   }else{
+    //If the text is not justified
+    //then just print the buffer followed by
+    //a new line.
     printf("%s\n", buffer);
   }
   return 1;
--- main.c
+++ main.c
@@ -1,3 +1,13 @@
+/*
+Dylan Strohschein
+CIS 343-02
+This program will print the input text
+with a max width with the parameter -w
+if the parameter -j is used all lines will
+be justified to the same width if its not
+the last line of a paragraph
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -26,6 +36,8 @@ int main(int argc, char *argv[])
       }
     }
 
+    //If there is a j as the argument we need to get the number after it and
+    //set justified equal to 1
     if (strcmp(argv[i], "-j") == 0)
     {
       if (i + 1 < argc)
@@ -41,21 +53,25 @@ int main(int argc, char *argv[])
     }
   }
 
+  //If there was no w or j value throw an error
   if(foundLegthValue == 0)
   {
     fprintf (stderr, "You must specify a -w or -j value");
     return -1;
   }
 
+  //If there was both a w and j value throw an error
   if(foundLegthValue == 2)
   {
     fprintf (stderr, "You must not specify both a -w and -j value");
     return -1;
   }
 
+  //Set up the current line to handle a max of a 100 character line
   char *curLine;
   curLine = (char*) malloc(101 * sizeof(char));
 
+  //Setup our buffer to be able to hold a string as long as the specified line length
   char *buffer;
   buffer = (char*) malloc((lineLength + 1) * sizeof(char));
   strcpy(buffer, "");
@@ -66,8 +82,11 @@ int main(int argc, char *argv[])
   int overflowLines = 0;
   int spacesInLine = 0;
 
+  //While there is another line coming from stdin
   while (LoadNextLine(&curLine))
   {
+    //If the line just equals a new line then we know
+    //this is the end of a paragraph
     if (strcmp(curLine, "\n") == 0)
     {
       printf("%s", buffer);
@@ -78,12 +97,24 @@ int main(int argc, char *argv[])
     }
     else
     {
+      //Split the current line by space and new line and loop
+      //through each word.
       char *curWord = strtok(curLine, " \n");
       while (curWord != NULL)
       {
-        if((isFirstInLine == 1 && curLineLength + strlen(curWord) <= lineLength) 
-        || (isFirstInLine == 0 && curLineLength + strlen(curWord) + 1 <= lineLength)){
+        //If the word can fit in the current line then 
+        //we will add it to the current buffer
+        if(
+            (isFirstInLine == 1 && 
+            curLineLength + strlen(curWord) <= lineLength) 
+            || 
+            (isFirstInLine == 0 && 
+            curLineLength + strlen(curWord) + 1 <= lineLength)
+          ){
           
+          //If the word is not the first in
+          //the line then we need to put a 
+          //space before it
           if(isFirstInLine == 0){
             buffer = strcat(buffer, " ");
             spacesInLine++;
@@ -92,34 +123,50 @@ int main(int argc, char *argv[])
             isFirstInLine = 0;
           }
 
+          //Add the current word to the buffer and update the
+          //current lines length
           curLineLength += strlen(curWord);
           buffer = strcat(buffer, curWord);
         }else{
+          //If the next word will not fit on the current
+          //line then we need to print the buffer
+          //to the screen and clear it
           PrintBuffer(buffer, justified, spacesInLine, lineLength);
           spacesInLine = 0;
           strcpy(buffer, "");
           curLineLength = strlen(curWord);
+
+          //If we can not fit the current word on
+          //its own line then we have an overflow error.
           if(curLineLength >= lineLength){
-            //Overflow Error
             overflowLines++;
             printf("%s", curWord);
           }else{
             strcpy(buffer, curWord);
           }
         }
+
+        //Move to the next word in the line
         curWord = strtok(NULL, " \n");
       }
     }
   }
 
+  //After we have gone through all the words we
+  //need to print the rest of the buffer
+  //to the screen. It is an incomplete
+  //line so we dont need any formatting.
   printf("%s", buffer);
 
+  //If there were any overflow lines
+  //tell the user.
   if(overflowLines > 0){
     fprintf (stderr, "\n\nWarning: %d overfull line(s)", overflowLines);
   }
 
+  //Release the pointers.
   free(curLine);
   free(buffer);
 
-  return 0;
+  return 1;
 }
